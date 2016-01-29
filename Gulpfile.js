4a9ff327ff86cd9a30bae43cc999fdd9b9bdf164
kirisu <github@kirisu.co.uk>
2016-01-29 20:46:43 +0000

    Added postcss.

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var reporter = require('postcss-reporter');
+var stylelint = require('stylelint');
+var scss = require('postcss-scss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('default', function () {
+  return gulp.src(['_frontend/sass/**/*.scss'])
+    .pipe(postcss([
+      stylelint(),
+      reporter({ clearMessages: true }),
+      autoprefixer({ browsers: ['last 2 versions'] })
+    ], { syntax: scss }))
+    .pipe( gulp.dest( '_sass' ) );
+});
